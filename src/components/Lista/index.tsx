121fa0feec42096b1f84f47f5fa43f88164ebf64
CaioHLuwi <caiohenriquelw2004@gmail.com>
2023-09-09 13:19:15 -0300

    refactor: inserindo a key na tag

--- src/components/Lista/index.tsx
+++ src/components/Lista/index.tsx
@@ -7,13 +7,16 @@ function Lista(){
     }, {
         tarefa: 'Javascript',
         tempo: '01:00:00'
+    }, {
+        tarefa: 'Typescript',
+        tempo: '03:00:00'
     }]
     return(
         <aside>
             <h2> Estudos do dia </h2>
             <ul>
                 {tarefas.map((item, index) => (
-                    <li>
+                    <li key={index}>
                         <h3> {item.tarefa}</h3>
                         <span> {item.tempo} </span>
                     </li>
