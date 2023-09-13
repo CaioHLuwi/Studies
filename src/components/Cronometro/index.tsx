2c2f10733a26972538139c9027b063d6258d15ad
CaioHLuwi <caiohenriquelw2004@gmail.com>
2023-09-13 12:33:36 -0300

    fix: erro de escrita

--- src/components/Cronometro/index.tsx
+++ src/components/Cronometro/index.tsx
@@ -5,7 +5,7 @@ import style from "./Cronometro.module.scss"
 export function Cronometro() {
     return (
         <div className={style.cronometro}>
-            <p className={style.titulo}>Escola um card e inicie o cronômetro</p>
+            <p className={style.titulo}>Escolha um card e inicie o cronômetro</p>
             <div className={style.relogioWrapper}>
                 <Relogio />
             </div>
