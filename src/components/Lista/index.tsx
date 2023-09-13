8e4152f0087b3f12c074c33cf56f38ebee82ff51
CaioHLuwi <caiohenriquelw2004@gmail.com>
2023-09-13 13:15:21 -0300

    refactor: recebendo tarefas via props do App.tsx

--- src/components/Lista/index.tsx
+++ src/components/Lista/index.tsx
@@ -1,23 +1,15 @@
-import React, { useState } from "react";
 import style from './Lista.module.scss';
 import Item from "./Item";
 
-function Lista(){
-    const [tarefas, setTarefas] = useState([{
-        tarefa: 'React', // useState = valor default
-        tempo: '02:00:00' // setTarefas = função para definir um novo estado, no onClick por exemplo
-    }, {
-        tarefa: 'Javascript',
-        tempo: '01:00:00'
-    }, {
-        tarefa: 'Typescript',
-        tempo: '03:00:00'
-    }])
+interface ITarefa {
+    tarefa: string,
+    tempo: string
+}
+
+function Lista({ tarefas }: { tarefas: ITarefa[]}){
     return(
         <aside className={style.listaTarefas}>
-            <h2 onClick={() => {
-                setTarefas([...tarefas, { tarefa: "Estudar estado", tempo: "05:00:00" }])
-            }}> Estudos do dia </h2>
+            <h2> Estudos do dia </h2>
             <ul>
                 {tarefas.map((item, index) => (
                     <Item
