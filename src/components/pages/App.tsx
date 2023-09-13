ad8ca8a110938350d608cd7ef123bc84f6273d0a
CaioHLuwi <caiohenriquelw2004@gmail.com>
2023-09-13 13:16:42 -0300

    refactor: transforma tarefa em escopo externo

--- src/components/pages/App.tsx
+++ src/components/pages/App.tsx
@@ -1,14 +1,24 @@
-import React from 'react';
+import React, { useState } from 'react';
 import Formulario from '../Formulario';
 import Lista from '../Lista';
 import style from './App.module.scss';
 import { Cronometro } from '../Cronometro';
 
 function App() {
+  const [tarefas, setTarefas] = useState([{
+    tarefa: 'React', // useState = valor default
+    tempo: '02:00:00' // setTarefas = função para definir um novo estado, no onClick por exemplo
+  }, {
+      tarefa: 'Javascript',
+      tempo: '01:00:00'
+  }, {
+      tarefa: 'Typescript',
+      tempo: '03:00:00'
+  }])
   return (
     <div className={style.AppStyle}>
-      <Formulario />
-      <Lista />
+      <Formulario setTarefas={setTarefas}/>
+      <Lista tarefas={tarefas}/>
       <Cronometro />
     </div>
   );
