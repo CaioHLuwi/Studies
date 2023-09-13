95a779656a07f69eca75547629990d1fd0b296d5
CaioHLuwi <caiohenriquelw2004@gmail.com>
2023-09-13 14:38:44 -0300

    feat: seta e typa o state para ser usado na props

--- src/components/pages/App.tsx
+++ src/components/pages/App.tsx
@@ -3,18 +3,10 @@ import Formulario from '../Formulario';
 import Lista from '../Lista';
 import style from './App.module.scss';
 import { Cronometro } from '../Cronometro';
+import { ITarefa } from '../../types/tarefa';
 
 function App() {
-  const [tarefas, setTarefas] = useState([{
-    tarefa: 'React', // useState = valor default
-    tempo: '02:00:00' // setTarefas = função para definir um novo estado, no onClick por exemplo
-  }, {
-      tarefa: 'Javascript',
-      tempo: '01:00:00'
-  }, {
-      tarefa: 'Typescript',
-      tempo: '03:00:00'
-  }])
+  const [tarefas, setTarefas] = useState<ITarefa[]>([])
   return (
     <div className={style.AppStyle}>
       <Formulario setTarefas={setTarefas}/>
